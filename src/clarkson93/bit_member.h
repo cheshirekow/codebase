3e5517d0067a0c9a79286fb1e25eabecc53c782b
Josh Bialkowski <josh.bialkowski@gmail.com>
2015-06-10 17:46:51 -0700

    add dox to bit_member.h

--- src/clarkson93/bit_member.h
+++ src/clarkson93/bit_member.h
@@ -21,23 +21,55 @@
 
 #include <bitset>
 
-namespace mpblocks {
 namespace clarkson93 {
 
 /// dummy class which allows us to use SNFINAE
 struct BitMemberBase {};
 
 /// indicates membership into a number of sets by a bitfield
-template <typename Enum, unsigned int Size>
-struct BitMember : public BitMemberBase, public std::bitset<Size> {
-  void addTo(Enum setBit) { (*this)[setBit] = true; }
+/**
+ *  There are several interesting sets that a particular simplex in the
+ *  triangulation may be a member of, particularly during certain walks and
+ *  searches. In several cases it is useful to indicate set membership within
+ *  the object itself. This class provides a thin wrapper around std::bitset
+ *  in order to reference individual set-membership bits by an index from an
+ *  enumeration.
+ *
+ *  For example:
+ *  @code
+    enum MySets {
+      SET_A = 0,
+      SET_B,
+      SET_C,
+      NUM_SETS
+    };
 
-  void removeFrom(Enum setBit) { (*this)[setBit] = false; }
+    BitMember<MySets,NUM_SETS> set_member;
+    set_member.AddTo(SET_A);
+    set_member.AddTo(SET_B);
+    bool is_member_of_a = set_member.IsMemberOf(SET_A); // true
+    bool is_member_of_b = set_member.IsMemberOf(SET_B); // true
+    bool is_member_of_c = set_member.IsMemberOf(SET_C); // false
+@endcode
+ */
+template <typename Enum, unsigned int size_>
+struct BitMember : public BitMemberBase, public std::bitset<size_> {
+  /// mark this object as a member of the @set_bit set
+  void AddTo(Enum set_bit) {
+    (*this)[set_bit] = true;
+  }
 
-  bool isMemberOf(Enum setBit) { return (*this)[setBit]; }
+  /// mark this object as not-a-member of the @set_bit set
+  void RemoveFrom(Enum set_bit) {
+    (*this)[set_bit] = false;
+  }
+
+  /// return true if this object is a member of the @set_bit set
+  bool IsMemberOf(Enum set_bit) const {
+    return (*this)[set_bit];
+  }
 };
 
 }  // namespace clarkson93
-}  // namespace mpblocks
 
 #endif  // CLARKSON93_BIT_MEMBER_H_
