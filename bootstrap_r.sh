7cffbdb9f5b570b597d2e86de64a9204ca34011c
Josh <josh@skyd.io>
2016-01-26 20:56:06 -0800

    add stub

--- bootstrap_r.sh
+++ bootstrap_r.sh
@@ -7,7 +7,6 @@ export CMAKE_PREFIX_PATH=$PREFIX:$CMAKE_PREFIX_PATH
 export PKG_CONFIG_PATH=$PREFIX/lib/pkgconfig/:$PKG_CONFIG_PATH
 cmake \
     -G "Eclipse CDT4 - Unix Makefiles" \
-    -DCMAKE_CXX_FLAGS="-std=c++11" \
     -DCMAKE_INSTALL_PREFIX=$PREFIX \
     -DCMAKE_PREFIX_PATH="$CMAKE_PREFIX_PATH" \
     -DCMAKE_BUILD_TYPE=Release \
