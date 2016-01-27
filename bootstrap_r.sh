51bee246fe74f93be3b21f40c31ac7982aa3150b
Josh <josh@skyd.io>
2016-01-26 20:59:45 -0800

    fix ninja build

--- bootstrap_r.sh
+++ bootstrap_r.sh
@@ -6,7 +6,7 @@ export SCRIPT_DIR=`dirname $0`;
 export CMAKE_PREFIX_PATH=$PREFIX:$CMAKE_PREFIX_PATH
 export PKG_CONFIG_PATH=$PREFIX/lib/pkgconfig/:$PKG_CONFIG_PATH
 cmake \
-    -G "Eclipse CDT4 - Unix Makefiles" \
+    -G "Eclipse CDT4 - Ninja" \
     -DCMAKE_INSTALL_PREFIX=$PREFIX \
     -DCMAKE_PREFIX_PATH="$CMAKE_PREFIX_PATH" \
     -DCMAKE_BUILD_TYPE=Release \
